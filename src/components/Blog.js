fbae220adb82710ba70e3a7d25a90d3f67a1076e
rosenstein.samuel1 <rosenstein.samuel1@gmail.com>
2020-06-02 14:43:01 +0300

    react-hooks-blog-lesson-1

---
+++ src/components/Blog.js
@@ -0,0 +1,8 @@
+import React from "react";
+import BlogForm from "./BlogForm";
+
+function Blog() {
+  return <BlogForm />;
+}
+
+export default Blog;
---
+++ src/components/BlogForm.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+function BlogForm(props) {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredContent, setEnteredContent] = useState("");
+
+  const submitHandler = event => {
+    event.preventDefault();
+    setEnteredTitle("");
+    setEnteredContent("");
+  };
+
+  return (
+    <section>
+      <form onSubmit={submitHandler}>
+        <div>
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            id="title"
+            value={enteredTitle}
+            onChange={event => {
+              setEnteredTitle(event.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <label htmlFor="content">Content</label>
+          <input
+            type="text"
+            id="content"
+            value={enteredContent}
+            onChange={event => {
+              setEnteredContent(event.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <button type="submit">Publish Post</button>
+        </div>
+      </form>
+    </section>
+  );
+}
+
+export default BlogForm;
